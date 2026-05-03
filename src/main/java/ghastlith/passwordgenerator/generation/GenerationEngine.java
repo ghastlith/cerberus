a9c0c739bf4d87a1a8069d208ec300f5ba87a2a3
ghastlith <72772611+ghastlith@users.noreply.github.com>
2026-05-02 23:34:57 -0300

    feat: add generation engine logic

--- src/main/java/ghastlith/passwordgenerator/generation/GenerationEngine.java
+++ src/main/java/ghastlith/passwordgenerator/generation/GenerationEngine.java
@@ -1,6 +1,14 @@
 package ghastlith.passwordgenerator.generation;
 
+import static ghastlith.passwordgenerator.generation.CharacterSet.LETTERS;
+import static ghastlith.passwordgenerator.generation.CharacterSet.NUMBERS;
+import static ghastlith.passwordgenerator.generation.CharacterSet.SPECIAL;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Collections;
 
 import org.springframework.stereotype.Component;
 import org.springframework.validation.annotation.Validated;
@@ -15,8 +23,40 @@ public class GenerationEngine {
 
   private final SecureRandom random;
 
+  private static final int RANDOM_LOWER_BOUND = 0;
+
   public String generatePassword(@Valid final GenerationPolicy policy) {
-    throw new RuntimeException("method not implemented");
+    final var letters = getRandomCharacters(LETTERS, policy.lettersLength());
+    final var numbers = getRandomCharacters(NUMBERS, policy.numbersLength());
+    final var special = getRandomCharacters(SPECIAL, policy.specialLength());
+
+    final var password = shuffleCharacters(letters, numbers, special);
+
+    return password;
+  }
+
+  private String shuffleCharacters(final String... parts) {
+    final var characters = Arrays.stream(parts)
+        .collect(joining())
+        .chars()
+        .mapToObj(c -> (char) c)
+        .collect(toList());
+
+    Collections.shuffle(characters);
+
+    return characters.stream()
+        .map(String::valueOf)
+        .collect(joining());
+  }
+
+  private String getRandomCharacters(final String set, final int length) {
+    final var upperBound = set.length();
+
+    return random.ints(RANDOM_LOWER_BOUND, upperBound)
+        .limit(length)
+        .mapToObj(set::charAt)
+        .map(String::valueOf)
+        .collect(joining());
   }
 
 }
